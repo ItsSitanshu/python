e5d3b124990a0bea436cbc688cf44ed0cfed4d21
ItsSitanshu <sitanshu15shrestha@gmail.com>
2021-09-25 14:36:43 +0545

    Make facerec work

--- Face rcongnition/facerec.py
+++ Face rcongnition/facerec.py
@@ -3,10 +3,18 @@ import cv2
 import numpy as np
 
 video_capture = cv2.VideoCapture(0)
+sheela_image = face_recognition.load_image_file("img/sheela.png")
+sheela_face_encoding = face_recognition.face_encodings(sheela_image)[0]
 
+sitanshu_image = face_recognition.load_image_file("img/sitanshu.png")
+sitanshu_face_encoding = face_recognition.face_encodings(sitanshu_image)[0]
 
-known_face_encodings = []
+known_face_encodings = [
+    sheela_face_encoding,
+    sitanshu_face_encoding
+]
 known_face_names = [
+    "Sheela",
     "Sitanshu"
 ]
 
@@ -24,7 +32,6 @@ while True:
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
         face_names = []
         for face_encoding in face_encodings:
-            
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
             name = "Unknown"
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
@@ -34,23 +41,22 @@ while True:
             face_names.append(name)
 
     process_this_frame = not process_this_frame
+
     for (top, right, bottom, left), name in zip(face_locations, face_names):
-        
         top *= 4
         right *= 4
         bottom *= 4
         left *= 4
-
-        
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-        
         cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+    
     cv2.imshow('Video', frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 
 video_capture.release()
 cv2.destroyAllWindows()
